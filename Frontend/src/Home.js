db56f6c9db04f6681bdb7d0b9f3ce483dcaeeda7
~ hehhehhe <athmikaubhat@gmail.com>
2024-03-16 00:07:50 +0530

    Add files via upload

---
+++ Frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import BG from './imgs/bg.jpg';
+const Home = () => {
+  return (
+    <>
+      
+      
+    </>
+  );
+};
+
+export default Home;
